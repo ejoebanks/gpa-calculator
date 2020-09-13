19f51e1f12ab2d1116921f0ea2e853f265ef5eba
Elijah <ejoebanks@gmail.com>
2020-09-12 21:51:51 -0500

    Added conditional rendering to remove row

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -7,12 +7,12 @@ export default function TableRow() {
     for (let i = 0; i < 4; i++) {
       tableRowGroups.push(
         <>
-          <td id={i}></td>
-          <td><input name="course" className="form-control" value='' placeholder="Course"/>{i}</td>
-          <td><input id={"Grade " + i + ""}  name="course" className="form-control" value='' placeholder="Grade" />{i}</td>
-          <td><input id={i}  name="course" className="form-control" value='' placeholder="Credit Hours" />{i}</td>
-          <td><input type="checkbox" className="btn_repeat" id={i} />{i}</td>
-          <td><input id={i} name="total" className="total form-control" id="total1" />{i}</td>
+          <td id={i}>{i > 0 && <button type="button" name="remove" id="" class="btn_remove btn">X</button>}</td>
+          <td><input name="course" className="form-control" value='' placeholder="Course"/></td>
+          <td><input id={"Grade " + i + ""}  name="course" className="form-control" value='' placeholder="Grade" /></td>
+          <td><input id={i}  name="course" className="form-control" value='' placeholder="Credit Hours" /></td>
+          <td><input type="checkbox" className="btn_repeat" id={i} /></td>
+          <td><input id={i} name="total" className="total form-control" id="total1" /></td>
         </>
     )
   }
