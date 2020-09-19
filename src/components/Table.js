d6eea9fcef952769d160c6755ee150c6767a79cb
Elijah <ejoebanks@gmail.com>
2020-09-19 09:58:54 -0500

    Redoing table logic (WIP), ID needs to be unique upon deletion/add

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,80 +1,78 @@
 import React, { useState, useEffect } from "react";
-import TableRow from './TableRow';
-import AddRow from './TableRow.js';
-import ButtonGroup from './ButtonGroup';
-import { row } from './ButtonGroup.js';
-
 
 const Table = () => {
-  const [rows, setRows] = useState([])
+  const [rows, setRows] = useState([]);
+  const tableRow = [
+    {
+      id: rows.length,
+      course: <input name="course" className="form-control" placeholder="Course"/>,
+      grade: <input id={"Grade_"}  name="course" className="form-control" placeholder="Grade" />,
+      credithours: <input name="credithours" className="form-control" value=" " placeholder="Credit Hours" />,
+      repeat: <input type="checkbox" className="btn_repeat" />,
+      total: <input name="total" className="total form-control" readOnly id="total1" />
+    }];
+  useEffect(() => {
+    getData();
+    }, [])
 
-  const removeData = (id) => {}
-
-  const renderHeader = () => {
-          let headerElement = ['remove', 'course', 'grade', 'credits', 'repeat', 'quality points']
-
-          return headerElement.map((key, index) => {
-              return <th key={index}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
-          })
-      }
-
-      const renderBody = (props) => {
-        for (let i = 0; i <= props; i++) {
-          rows.push(
-            <>
-              <td id={i}>{i > 0 && <button type="button" name="remove" id="" class="btn_remove btn">X</button>}</td>
-              <td><input name="course" className="form-control" value='' placeholder="Course"/></td>
-              <td><input id={"Grade_" + i + ""}  name="course" className="form-control" value='' placeholder="Grade" /></td>
-              <td><input id={i}  name="course" className="form-control" value='' placeholder="Credit Hours" /></td>
-              <td><input type="checkbox" className="btn_repeat" id={i} /></td>
-              <td><input id={i} name="total" className="total form-control" readOnly id="total1" /></td>
-            </>
-        )
-
-      }
-
-      return rows && rows.map((row, index) => {
-          return (
-            <tr id={index} key={index}>{row}</tr>
-          )
-      })
+  const getData =  () => {
+    setRows(tableRow);
   }
+    const renderHeader = () => {
+      let headerElement = ['remove', 'course', 'grade', 'credits',
+      'repeat', 'quality points']
 
-  let tableRowGroups = [];
-  for (let i = 0; i < 4; i++) {
-    tableRowGroups.push(
-      <tr>
-        <td id={i}></td>
-        <td><input id={i} name="course" className="form-control" value='' placeholder="Class Name"/>{i}</td>
-        <td><input id={i}  name="course" className="form-control" value='' placeholder="Grade" />{i}</td>
-        <td><input id={i}  name="course" className="form-control" value='' placeholder="Credit Hours" />{i}</td>
-        <td><input type="checkbox" className="btn_repeat" id={i} />{i}</td>
-        <td><input id={i}  name="total" className="total form-control" id="total1" />{i}</td>
-      </tr>
-  )
-}
+      return headerElement.map((key, index) => {
+        return <th key={index}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
+        })
+       }
 
-  let inputgroups = [];
+       const removeData = (id) => {
+         const del = rows.filter(row => id !== row.id)
+         setRows(del);
+       }
 
+       const addData = (id) => {
+         let add = [];
+         if (rows.length < 8) {
+           add = rows.concat(tableRow);
+           setRows(add);
+           alert(rows.length);
+         } else {
+           alert ("Course load is limited to 8");
+         }
+       }
 
-  for (let i = 0; i < 3; i++) {
-    inputgroups.push(
-        <TableRow key={i} />
-    )
-}
+       const renderBody = () => {
+           return rows && rows.map(({ id, course, repeat, credithours, grade, remove, total }) => {
+               return (
+                   <tr id={id} key={id}>
+                       <td>{id > 0 ? <button type="button" name="remove" id=" " onClick={() => removeData(id)}class="btn_remove btn">X</button> : " "}</td>
+                       <td>{course}</td>
+                       <td>{grade}</td>
+                       <td>{credithours}</td>
+                       <td>{repeat}</td>
+                       <td>{total}</td>
+                   </tr>
+               )
+           })
+       }
 
-
-const inputOut = inputgroups.map((row) => <div key={5}>{row}</div>);
 
   return (
+    <>
     <table className="table table-striped table-bordered">
       <thead>
         <tr>
         {renderHeader()}
         </tr>
       </thead>
-      {renderBody(3)}
+      <tbody>
+        {renderBody()}
+      </tbody>
     </table>
+    <button type="button" name="remove" id=" " onClick={() => addData()}class="btn_remove btn">X</button>
+    </>
   );
 }
 
