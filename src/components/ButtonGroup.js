80795679de2bcca98643a9bd31a756c6ec0772d7
Elijah <ejoebanks@gmail.com>
2020-09-20 11:53:39 -0500

    Moved add row to table component

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -1,28 +1,33 @@
 import React, { useState }  from "react";
 import Constants from '../Constants/Constants.js';
-
-function AddRow(event) {
-  let inputgroups = [];
-  let i = document.getElementById('#0');
-  console.log(i);
-}
+import Table from './Table.js';
 
 function freset() {
   document.getElementById("myform").reset();
   console.log( Constants.termGPA );
 }
 
+
 export default function ButtonGroup() {
+
+  const [count, setCount] = useState(null);
+  const row = [];
+
+  for (var i = 0; i < count; i += 1) {
+    row.push(<div key={i}>FFFFFFFFAAAAAAAAAA</div>);
+  };
 
   return (
     <div className="d-flex justify-content-center">
+    {row}
     <div className="btn-group mr-2" role="group" aria-label="Second group">
-    <button type="button" className="btn btn-secondary" onClick={AddRow}>Add Row</button>
+    <button type="button" className="btn btn-secondary" onClick={() => setCount(count + 1)}>Add Row</button>
     </div>
     <div className="btn-group mr-2" role="group" aria-label="Second group">
       <button type="button" className="btn btn-secondary resetBtn" onClick={freset}>Reset</button>
     </div>
     </div>
 
- )
+ );
+
 }
